8c20191c57f3aec507d9636ce9d8d5c2302f3de6
mazzegaDev <=>
2025-10-08 17:04:28 -0300

    construção do layout

---
+++ portfolio/app/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import style from "./style.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Meu Portfolio</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#sobre">Sobre mim</a>
+          </li>
+          <li>
+            <a href="#stack">Stack</a>
+          </li>
+          <li>
+            <a href="#projetos">Projetos</a>
+          </li>
+          <li>
+            <a href="#contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ portfolio/app/page.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+
+import style from "./style.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
